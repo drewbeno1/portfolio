89d4f595c6ee3267282e5d6f246426c11b351fcc
drewbeno1 <benodrew12@gmail.com>
2024-09-04 16:33:51 -0400

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software Engingeer and outdoor enthusiast.
+            Software Engineer and outdoor enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Drew, a software engineer based in south central Florida, USA. I have had the joy of focusing on agricultural technology.
